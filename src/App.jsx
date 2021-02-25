0e7ba8bccee7e38cecc69d237ff33c37f71af450
Aung Myo Htet <amh.rav@gmail.com>
2021-02-25 16:04:49 +0630

    ch3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,97 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe', 'America'];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
+const issues = [
+  {
+    id: 1,
+    status: "New",
+    owner: "Ravan",
+    effort: 5,
+    created: new Date("2018-08-15"),
+    due: undefined,
+    title: "Error in console when clicking Add",
+  },
+  {
+    id: 2,
+    status: "Assigned",
+    owner: "Eddie",
+    effort: 14,
+    created: new Date("2018-08-16"),
+    due: new Date("2018-08-30"),
+    title: "Missing bottom border on panel",
+  },
+];
 
-const element = (
-			<div title='Outer div'>
-				<h1>{message}</h1>
-			</div>
-			);
+class IssueFilter extends React.Component {
+  render() {
+    return <div>This is the placeholder for IssueFilter</div>;
+  }
+}
 
-ReactDOM.render(element, document.getElementById('content'));
+class IssueRow extends React.Component {
+  render() {
+      const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ""}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
 
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = issues.map((issue) => (
+      <IssueRow rowStyle={rowStyle} issue={issue} />
+    ));
+    const rowStyle = {
+      border: "1px solid silver",
+      padding: 4,
+    };
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is the placeholder for IssueAdd</div>;
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <hr />
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+        <hr />
+      </React.Fragment>
+    );
+  }
+}
+
+const element = <IssueList />;
+ReactDOM.render(element, document.getElementById("contents"));
