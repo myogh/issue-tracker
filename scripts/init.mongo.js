cf6d3d3635c55f5b4eb03ee2292ca2f6af3f0d95
Aung Myo Htet <amh.rav@gmail.com>
2021-04-08 09:55:51 +0630

    Initialize schema script for the mongo shell

---
+++ scripts/init.mongo.js
@@ -0,0 +1,40 @@
+// performing mongo shell operations
+// in a non-interactive way for the
+// creation of indexes and database initialization
+
+db.issues.remove({});
+
+const issuesDB = [
+    {
+        id: 1,
+        status: "New",
+        owner: "Ravan",
+        effort: 5,
+        created: new Date("2019-01-15"),
+        due: undefined,
+        title: "Error in console when clicking Add",
+    },
+    {
+        id: 2,
+        status: "Assigned",
+        owner: "Eddie",
+        effort: 14,
+        created: new Date("2019-01-16"),
+        due: new Date("2019-02-01"),
+        title: "Missing bottom border on panel",
+    },
+];
+
+db.issues.insertMany(issuesDB);
+const count = db.issues.count();
+print("Inserted", count, "issues");
+
+// {id: 1} is the field we would like to index
+// 1 specifies the index being an ascending one
+// {unique: true} creates a unique index which will reject...
+// any sort of duplication on that index
+
+db.issues.createIndex({ id: 1 }, { unique: true });
+db.issues.createIndex({ status: 1 });
+db.issues.createIndex({ owner: 1 });
+db.issues.createIndex({ created: 1 });
