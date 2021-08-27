0cd8236bc638e3f6cae39473be71af0780bcf719
Aung Myo Htet <amh.rav@gmail.com>
2021-08-27 13:26:50 +0630

    Add issue count api
    Implement a graphql api which fetches a summary of issues from
    MongoDB server using aggregation pipeline in order to show stats of
    issue owners and status counts in the IssueReport page.

--- api/issue.js
+++ api/issue.js
@@ -58,6 +58,40 @@ async function add(_, { issue }) {
   return savedIssue;
 }
 
+// --------- resolver func for issueCounts field ------------------
+
+async function counts(_, { status, effortMin, effortMax }) {
+  const db = getDb();
+  const filter = {};
+  if (status) filter.status = status;
+
+  if (effortMin !== undefined || effortMax !== undefined) {
+    filter.effort = {};
+    if (effortMin !== undefined) filter.effort.$gte = effortMin;
+    if (effortMax !== undefined) filter.effort.$lte = effortMax;
+  }
+
+  const results = await db.collection('issues').aggregate([
+    { $match: filter },
+    {
+      $group: {
+        _id: { owner: '$owner', status: '$status' },
+        count: { $sum: 1 },
+      },
+    },
+  ]).toArray();
+
+  const stats = {};
+  results.forEach((result) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const { owner, status: statusKey } = result._id;
+    if (!stats[owner]) stats[owner] = { owner };
+    stats[owner][statusKey] = result.count;
+  });
+
+  return Object.values(stats);
+}
+
 // ------------- resolver func for issueUpdate field --------
 
 async function update(_, { id, changes }) {
@@ -94,4 +128,5 @@ module.exports = {
   get,
   update,
   remove,
+  counts,
 };
